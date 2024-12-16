5c08ba4d3baed6eb6836db4b654e3c3d24053328
mani <manirazeghi049@gmail.com>
2024-12-16 00:27:04 -0800

    add_hint

--- game.py
+++ game.py
@@ -1,8 +1,16 @@
 from random import randint
 
 pc = randint(1, 100)
-print(pc)
+
+
 while (ask := int(input('guss number: '))) != pc:
+    
+    if ask > pc:
+        print('your guss is upper!')
+    
+    else:
+        print('your guss is lower!')
+    
     ask
 
 print('good job!')
