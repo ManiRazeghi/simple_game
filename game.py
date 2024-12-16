1648a48245ab22433723bd66d33bbe2e28c88ed1
ManiRazeghi <manirazeghi049@gmail.com>
2024-12-16 00:40:08 -0800

    add_heart

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from random import randint
 
 pc = randint(1, 100)
 guss_count = 0
+heart = int(input('heart: ')
 
 
 while (ask := int(input('guss number: '))) != pc:
@@ -13,7 +14,12 @@ while (ask := int(input('guss number: '))) != pc:
         print('your guss is lower!')
     
     guss_count += 1
+    heart -= 1
+
+    if heart == 0:
+        print(f'game over! answer: {pc}')
+        break
     
     ask
 
-print(f'good job! your guss count is: {guss_count}')
+print(f'your guss count is: {guss_count}')
