5c5c977c02afed0d05830be1a33a077dcb8481de
mani <manirazeghi049@gmail.com>
2024-12-16 01:09:44 -0800

    fix_game.py

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 from random import randint
 import os
+import time
 
 def game(heart: int, pc) -> None:
     guss_count = 0
     
     while (ask := int(input('guss number: '))) != pc:
         
-        print('your guss is upper!' if ask > pc else print('your guss is lower!'))
+        print('your guss is upper!' if ask > pc else 'your guss is lower!')
            
         guss_count += 1
         heart -= 1
@@ -24,7 +25,8 @@ while (ask := input('play?(y/n): ').lower()) == 'y':
    game(int(input('heart: ')), randint(1, 100))
    ask
 
-print('have a good day!)
+print('have a good day!')
+time.sleep(0.5)
 os.system('cls')
 
 
