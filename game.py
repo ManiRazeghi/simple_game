d80f2f5ca74d155115634da8e181962d70406be7
mani <manirazeghi049@gmail.com>
2024-12-16 00:29:47 -0800

    add_guss_count

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
 from random import randint
 
 pc = randint(1, 100)
+guss_count = 0
 
 
 while (ask := int(input('guss number: '))) != pc:
-    
+
     if ask > pc:
         print('your guss is upper!')
     
     else:
         print('your guss is lower!')
     
+    guss_count += 1
+    
     ask
 
-print('good job!')
+print(f'good job! your guss count is: {guss_count}')
